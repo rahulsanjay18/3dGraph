6d26c7c76a975a505ea129f4f01b56757d638c91
rahulsanjay18 <rahulsanjay18@gmail.com>
2018-04-16 15:23:48 -0400

    Readded files

---
+++ release/CMakeFiles/3dGrapher.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/3dGrapher.dir/main.cpp.o"
+  "bin/3dGrapher.pdb"
+  "bin/3dGrapher"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3dGrapher.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
