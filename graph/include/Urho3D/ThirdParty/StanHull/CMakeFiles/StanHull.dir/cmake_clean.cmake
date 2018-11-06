63c374ead142aed7860d7eeaffcd23ad57832e28
Hunter Schuler <schuler.hunter.l@gmail.com>
2018-11-05 23:27:08 -0500

    Removed redundant folder (the graph folder in each one), moved the URHO3D folder to just graph, and added the source files for URHO so no more referencing the home directory

---
+++ graph/include/Urho3D/ThirdParty/StanHull/CMakeFiles/StanHull.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/StanHull.dir/hull.cpp.o"
+  "../../../bin/libStanHull.pdb"
+  "libStanHull.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/StanHull.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ graph/include/Urho3D/librevision.h
@@ -0,0 +1,1 @@
+const char* revision="1.7-603-g024e1104e";
